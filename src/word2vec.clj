efed803b6a9d4bcc8f4342123af3f8f441716d0f
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-05-12 13:58:15 +0100

    Cleaner code

--- src/word2vec.clj
+++ src/word2vec.clj
@@ -4,32 +4,14 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]))
 
-(defn read-filtered-dataset
-  [inpfile]
-  (with-open [r (io/reader (java.util.zip.GZIPInputStream.
-                  (io/input-stream inpfile)))]
-    (mapv edn/read-string (line-seq r))))
+;; A textual document to learn from.
+(def data (create-input-format "textualData.txt"))
 
-(def data
-  (create-input-format "ulysses.txt"))
-
+;; Train the model
 (def model (clojure-word2vec.core/word2vec data :window-size 15))
 
-(println (take 20 (.getVocab model)))
+;; Print the most important features and their embedded words
+(def main-features (remove [] (.getVocab model)))
 
-(def matches (get-matches model "woman"))
-
-(def appvec
-  (-> (read-filtered-dataset "resources/apple-data.txt.gz") clojure-word2vec.core/word2vec))
-
-(println matches)
-
-(print (take 20 (.getVocab appvec)))
-
-(println (get-matches appvec "radeon"))
-(println (get-matches appvec "seagate"))
-(println (get-matches appvec "nano"))
-(println (get-matches appvec "projector"))
-(println (get-matches appvec "raid"))
-(println (get-matches appvec "quicktime"))
-(println (get-matches appvec "powermac"))
+(doseq [feature main-features]
+  (println feature ":" (get-matches model feature)))
