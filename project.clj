f2edc43e6e0929b541770444f3f26852b4f2854d
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-05-15 16:03:22 +0100

    Parameter parsing

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject word2vec "SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.bridgei2i/word2vec "0.1.0"]])
+                 [org.bridgei2i/word2vec "0.1.0"]
+                 [org.clojure/tools.cli "0.3.1"]])
--- src/word2vec.clj
+++ src/word2vec.clj
@@ -1,17 +1,28 @@
 (ns word2vec
   (:require [clojure-word2vec.core :refer [create-input-format get-matches]]  			                      
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class))
 
-(def data-file "textualData.txt")
+(defn run
+	[data-file num-features]
+	(let [data (create-input-format data-file) ;; Format input data
+		  model (clojure-word2vec.core/word2vec data) ;; Train the model
+		  main-features (take num-features (.getVocab model))] 
 
-;; Format input data
-(def data (create-input-format data-file))
+		;; Print the most important features and their embedded words  
+		(doseq [feature main-features]
+  			(println feature ":" (get-matches model feature)))))
 
-;; Train the model
-(def model (clojure-word2vec.core/word2vec data))
+(def cli-options
+  [["-f" "--file DATA-FILE" "Textual data file" :default "./textualData.txt"]
+   ["-n" "--num-features NUM-FEATURES" "Number of features to print" :default 100]
+   ["-h" "--help"]])
 
-;; Print the most important features and their embedded words
-(def main-features (take 200 (.getVocab model)))
-
-(doseq [feature main-features]
-  (println feature ":" (get-matches model feature)))
+(defn -main [& args]
+	(let [opts (parse-opts args cli-options)]
+		  (if (:help (:options opts))
+		  	(println (:summary opts))		  
+		  	(let [data-file (:file (:options opts))
+		  		  num-features (:num-features (:options opts))]
+			(run data-file (int num-features))))))
